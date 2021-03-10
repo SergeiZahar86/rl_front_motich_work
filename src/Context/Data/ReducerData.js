c34e958dc45d7f8981cd995de484c0557143b100
Сергей Захаров <sergei86zahar@mail.ru>
2021-03-10 12:03:08 +0700

    1

--- src/Context/Data/ReducerData.js
+++ src/Context/Data/ReducerData.js
@@ -1,5 +1,12 @@
 const ReducerData = (state, action) => {
     switch (action.type){
+        case "FETCH_NEWS":
+            console.log(action)
+            return {
+                ...state,
+                news: action.payload
+            }
+
         default:
             return state
     }
--- src/routes/News/News.js
+++ src/routes/News/News.js
@@ -2,8 +2,37 @@ import React from 'react'
 import Navbar from "../../modules/navigation/Navbar";
 import classes from "./../../style.module.scss";
 import Header from "../../modules/navigation/Header";
+import ContextData from "../../Context/Data/ContextData";
 
 const News = () => {
+
+	const {stateData, dispatchData} = React.useContext(ContextData)
+	console.log(stateData)
+	const news = stateData.news
+	React.useEffect(() => {
+		const fetchNews = async () => {
+			try{
+				const response = await fetch('http://localhost:3001/news')
+				if (response.ok) { // если HTTP-статус в диапазоне 200-299
+				                   // получаем тело ответа (см. про этот метод ниже)
+					let result = await response.json();
+					//console.log(result)
+					dispatchData({
+						type: "FETCH_NEWS",
+						payload: result
+					})
+				} else {
+					alert("Ошибка HTTP: " + response.status);
+				}
+			}catch (e){
+				//console.log(e)
+			}
+		}
+		fetchNews()
+	},[])
+
+
+
 	return(
 		<div className={classes.main}>
 			<div className={classes.main_navigation}>
@@ -11,7 +40,7 @@ const News = () => {
 			</div>
 			<div className={classes.main_content}>
 				<Header/>
-				News
+				{news.map(() => {})}
 			</div>
 		</div>
 	)
